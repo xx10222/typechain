0d888aefeff64db7643685f3f649a7dd7c7a1638
xx10222 <yjw4960@gmail.com>
2021-08-26 02:38:40 +0900

    #6. Interfaces on Typescript

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const person = {
+    name: "yummy",
+    age: 22,
+    gender: "female"
+};
+const sayHi = (person) => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+// console.log(sayHi("Nicolas", 24, "male"));
+console.log(sayHi(person));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("hello");
+/* const sayHi = (name: string, age: number, gender: string): string => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}!`; */
+  interface Human {
+    name: string;
+    age: number;
+    gender: string;
+  } 
+  
+  const person = {
+    name: "yummy",
+    age: 22,
+    gender: "female"
+  };
+  
+  const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${
+      person.gender
+    }!`;
+  };
+  
+  // console.log(sayHi("Nicolas", 24, "male"));
+  console.log(sayHi(person));
+  
+  export {};
