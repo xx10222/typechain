823bfa57281383e58c44b37df71589473934de9a
xx10222 <yjw4960@gmail.com>
2021-08-26 03:17:29 +0900

    #7 Classes on TS part 1

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 "use strict";
+/* const sayHi = (name: string, age: number, gender: string): string => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}!`; */
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "yummy",
-    age: 22,
-    gender: "female"
-};
+/*interface Human {
+  name: string;
+  age: number;
+  gender: string;
+} */
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const lynn = new Human("yummy", 22, "female");
+/*const person = {
+  name: "yummy",
+  age: 22,
+  gender: "female"
+};*/
 const sayHi = (person) => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 // console.log(sayHi("Nicolas", 24, "male"));
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,16 +1,30 @@
 /* const sayHi = (name: string, age: number, gender: string): string => {
     return `Hello ${name}, you are ${age}, you are a ${gender}!`; */
-  interface Human {
+
+  /*interface Human { //interface는 js 코드에선 보이지 않는다(Ts의 특성!)
     name: string;
     age: number;
     gender: string;
-  } 
+  } */
+
+  class Human {
+      public name: string; //Ts에선 private으로 관리 : 외부 속성에 노출 방지 가능!
+      public age: number;
+      public gender: string;
+      constructor(name: string, age: number, gender: string){
+          this.name=name;
+          this.age=age;
+          this.gender=gender;
+      }
+  }
+
+  const lynn = new Human("yummy",22,"female");
   
-  const person = {
+  /*const person = {
     name: "yummy",
     age: 22,
     gender: "female"
-  };
+  };*/
   
   const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${
@@ -19,6 +33,6 @@
   };
   
   // console.log(sayHi("Nicolas", 24, "male"));
-  console.log(sayHi(person));
+  console.log(sayHi(lynn));
   
   export {};
